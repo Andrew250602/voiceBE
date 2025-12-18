460db1b6ef1d7c88aabecaed67532fdc8e35eaae
trieuvy <trieuvy.nguyen@vn.wilmar-intl.com>
2025-12-18 11:49:58 +0700

    ci-cd test 78

--- src/main/java/com/wilmar/web/controller/Health/HealthController.java
+++ src/main/java/com/wilmar/web/controller/Health/HealthController.java
@@ -1,11 +1,13 @@
 package com.wilmar.web.controller.Health;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/health")
 public class HealthController {
-    @GetMapping("/api/health")
+    @GetMapping("/")
     public String checkHealth() {
         return "Backend is running!";
     }
